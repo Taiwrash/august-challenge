da32f27f8f5cd8d2613ab675473ac2349356cb2e
Mudasiru Rasheed <rasheedrtm1@gmail.com>
2020-08-10 22:58:54 +0100

    committed

---
+++ day2/app.js
@@ -0,0 +1,20 @@
+const slider = document.getElementById("mode");
+let content = document.querySelectorAll("div");
+let header = document.querySelector("h3");
+let header1 = document.querySelector("h4");
+
+slider.addEventListener("change", () => {
+  slider.checked
+    ? content.forEach((contents) => {
+        contents.classList.add("dark-theme");
+        header.classList.add("dark-theme");
+        header1.classList.add("dark-theme");
+        slider.classList.add("dark-theme");
+      })
+    : content.forEach((contents) => {
+        contents.classList.remove("dark-theme");
+        header.classList.remove("dark-theme");
+        header1.classList.remove("dark-theme");
+        slider.classList.remove("dark-theme");
+      });
+});
