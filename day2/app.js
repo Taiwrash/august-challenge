e166c4995959e45bd783f6186628282fff91c94c
Mudasiru Rasheed <rasheedrtm1@gmail.com>
2020-08-11 23:07:00 +0100

    Update

--- day2/app.js
+++ day2/app.js
@@ -2,7 +2,7 @@ const slider = document.getElementById("mode");
 let content = document.querySelectorAll("div");
 let header = document.querySelector("h3");
 let header1 = document.querySelector("h4");
-const body = document.querySelector("body");
+let body = document.querySelector("body");
 
 slider.addEventListener("change", () => {
   slider.checked
@@ -14,11 +14,13 @@ slider.addEventListener("change", () => {
         header1.classList.add("dark-theme");
         slider.classList.add("dark-theme");
       })
-    : content.forEach((contents) => {
+    : 
+         body.classList.remove("dark-theme");
+        content.forEach((contents) => {
         contents.classList.remove("dark-theme");
         header.classList.remove("dark-theme");
         header1.classList.remove("dark-theme");
         slider.classList.remove("dark-theme");
-        body.classList.remove("dark-theme");
+       
       });
 });
