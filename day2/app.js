e9bf0dec72f6faf6412397aba63b71cbfb96ff46
Mudasiru Rasheed <rasheedrtm1@gmail.com>
2020-08-11 22:13:36 +0100

    Update app

--- day2/app.js
+++ day2/app.js
@@ -2,10 +2,13 @@ const slider = document.getElementById("mode");
 let content = document.querySelectorAll("div");
 let header = document.querySelector("h3");
 let header1 = document.querySelector("h4");
+const body = document.querySelector("body")
 
 slider.addEventListener("change", () => {
   slider.checked
-    ? content.forEach((contents) => {
+    ?
+        body.classList.add("dark-theme");
+        content.forEach((contents) => {
         contents.classList.add("dark-theme");
         header.classList.add("dark-theme");
         header1.classList.add("dark-theme");
@@ -16,5 +19,6 @@ slider.addEventListener("change", () => {
         header.classList.remove("dark-theme");
         header1.classList.remove("dark-theme");
         slider.classList.remove("dark-theme");
+        body.classList.remove("dark-theme")
       });
 });
